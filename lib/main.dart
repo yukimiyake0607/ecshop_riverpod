b0869bd237f6f6f8787898a5990c6624f3206415
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-09 18:00:44 +0900

    add: SystemChrome.setPreferredOrientationsメソッドを使用
    refs デバイスを横にしても、画面を回転しないように設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ecshop_riverpod/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MainApp());
 }
-
-
