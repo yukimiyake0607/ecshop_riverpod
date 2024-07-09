dda668f6363a23eff4bc3f8c792eb1ec3e85ca88
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-09 16:42:52 +0900

    add: 『ホーム画面の枠組み完成』home_itemsをスタイルを作成
    refs ここからリファクター

--- lib/screens/home_screen/home_items.dart
+++ lib/screens/home_screen/home_items.dart
@@ -1,27 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeItems extends StatelessWidget {
   const HomeItems({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SliverGrid(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          return Container(
-            color: Colors.lightBlueAccent,
-            alignment: Alignment.center,
-            child: Text('Home Item$index'),
-          );
-        },
-        childCount: 10,
-      ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-        childAspectRatio: 0.8,
+    return SliverPadding(
+      padding: const EdgeInsets.all(8),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return _ItemsCard(id: index);
+          },
+          childCount: 10,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+          childAspectRatio: 0.8,
+        ),
       ),
     );
+  }
+}
+
+class _ItemsCard extends StatelessWidget {
+  _ItemsCard({super.key, required this.id});
+
+  int id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.width / 2 * 0.8,
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: NetworkImage(
+                'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1625&q=80',
+              ),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(5),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                spreadRadius: 0.2,
+                offset: Offset(1, 1),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2),
+          child: Text(
+            "Super Shoes 1",
+            style: GoogleFonts.montserrat(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2),
+          child: Text(
+            '12,800yen',
+            style: GoogleFonts.montserrat(),
+          ),
+        ),
+      ],
+    );
+    ;
   }
 }
