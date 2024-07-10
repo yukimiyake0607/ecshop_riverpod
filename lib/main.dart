b05683ccef0d311dff91a03dd843f089f264b344
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-10 15:43:45 +0900

    add: ProviderScopeでラップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:ecshop_riverpod/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
