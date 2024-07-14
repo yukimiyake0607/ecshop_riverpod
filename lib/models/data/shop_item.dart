906eda98aa7d35b1168b4ba1e825ed911d7f6388
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-14 22:00:13 +0900

    docs: コメントアウト削除

--- lib/models/data/shop_item.dart
+++ lib/models/data/shop_item.dart
@@ -6,7 +6,6 @@ import 'package:ecshop_riverpod/models/data/item/item.dart';
 final shopItemProvider = FutureProvider((ref) async {
   final data = await rootBundle.loadString('lib/mock/mock.json');
   final json = (await jsonDecode(data) as List).cast<Map<String, dynamic>>();
-  // ↑ここまで理解
   final items = json.map(Item.fromJson).toList();
   return items;
 });
