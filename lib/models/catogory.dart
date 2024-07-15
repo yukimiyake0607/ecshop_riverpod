2b91588c372c52a8497e25bab0091b875fbed522
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-15 20:27:23 +0900

    add: categoryIconPathMapProviderを作成
    refs 画像パスをProviderを管理するため

--- lib/models/catogory.dart
+++ lib/models/catogory.dart
@@ -10,3 +10,18 @@ enum ItemCategory {
 }
 
 final selectedCategoryProvider = StateProvider((ref) => ItemCategory.shoes);
+
+final categoryIconPathMapProvider = Provider<Map<ItemCategory, String>>((ref) {
+  return {
+    ItemCategory.shoes: 'assets/icons/shoes.png',
+    ItemCategory.shirts: 'assets/icons/shirts.png',
+    ItemCategory.jacket: 'assets/icons/jacket.png',
+    ItemCategory.pants: 'assets/icons/pants.png',
+    ItemCategory.eyewear: 'assets/icons/eyewear.png',
+    ItemCategory.bag: 'assets/icons/bag.png',
+  };
+}); 
+
+final categoryIconPathProvider = Provider.family((ref, ItemCategory category) {
+  return ref.watch(categoryIconPathMapProvider)[category];
+});
